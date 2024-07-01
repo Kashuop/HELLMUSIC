4add0574e7943af4e5879b692fd789c0ff9c1610
Kashuop <kashusahuop@gmail.com>
2024-07-02 01:38:18 +0530

    active.py

--- AviaxMusic/plugins/tools/active.py
+++ AviaxMusic/plugins/tools/active.py
@@ -75,8 +75,3 @@ async def activevi_(_, message: Message):
             disable_web_page_preview=True,
         )
         
-@app.on_message(filters.command("ac") & SUDOERS)
-async def start(client: Client, message: Message):
-    ac_audio = str(len(await get_active_chats()))
-    ac_video = str(len(await get_active_video_chats()))
-    await message.reply_text(f"✫ <b><u>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</u></b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('↻ 𝗖ʟᴏsᴇ  ↻', callback_data=f"close")]])))
